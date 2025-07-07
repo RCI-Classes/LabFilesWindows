f66d3054e32f0efd39309133a2e0da1751e6dbb9
AuditClay <42394776+AuditClay@users.noreply.github.com>
2025-07-07 06:47:01 -0500

    Pester updates

--- pester/Windows.Labs.tests.ps1
+++ pester/Windows.Labs.tests.ps1
@@ -33,33 +33,6 @@ Describe '507 Labs' {
       }
     }
 
-    #If the Azure configuration is not there, then skip the Azure tests
-    if (Test-Path -Path C:\Users\student\.azure\azureProfile.json) {
-      $azSubCount = (Get-Content C:\Users\student\.azure\azureProfile.json | ConvertFrom-Json).Subscriptions.Count
-    }
-    else {
-      $azSubCount = 0
-    }
-    
-    if ( $azSubCount -lt 1) {
-      Write-Host "Skipping Azure tests because config files do not exist"
-      $skipAzure = $true
-    } 
-    else {
-      Write-Host 'Importing AZ Accounts module'
-      Import-Module Az.Accounts
-      Write-Host 'Import complete'
-
-      Write-Host 'Importing AZ Compute module'
-      Import-Module Az.Compute
-      Write-Host 'Import complete'
-
-      if ((Get-AzTenant).Name -notlike '*RCI*') {
-        Write-Host "Skipping Azure tests because tenant is not correct"
-        $skipAzure = $true
-      }
-    }
-    
     #Check if alma is reachable
     if ( -not (Test-NetConnection -InformationLevel Quiet -ComputerName alma.lab.local) ) {
       Write-Host "Skipping alma tests because host is unreachable"
@@ -75,8 +48,6 @@ Describe '507 Labs' {
     if ( -not (Test-NetConnection -ComputerName 10.50.7.23 -Port 80 -InformationLevel Quiet) ) {
       $skipWeb = $true
     }
-
-
   }
 
   Context 'Lab 1.3 - AWS' -Skip:$skipAWS {
@@ -85,28 +56,28 @@ Describe '507 Labs' {
       $username | Should -BeLike '*AMartinez*' 
     }
 
-    It 'Part 3 - AWS CLI returns instances' {
+    It 'Part 1 - AWS CLI returns instances' {
       $instanceCount = ((aws ec2 describe-instances --profile default | ConvertFrom-Json).Reservations.Count)
       $instanceCount | Should -BeGreaterOrEqual 5 
     }
 
-    It 'Part 3 - PowerShell module returns instances' {
+    It 'Part 1 - PowerShell module returns instances' {
       (Get-EC2Instance).Count | Should -BeGreaterOrEqual 5
     }
 
-    It 'Part 3 - Get-AWSCmdletName returns multiple results' {
+    It 'Part 1 - Get-AWSCmdletName returns multiple results' {
       (Get-AWSCmdletName -ApiOperation describeinstances).Count | 
         Should -BeGreaterOrEqual 3
     }
 
-    It 'Part 3 - Get-AWSCmdletName with service returns correct results' {
+    It 'Part 1 - Get-AWSCmdletName with service returns correct results' {
       (Get-AWSCmdletName -ApiOperation describeinstances -Service compute).CmdletName | 
         Should -Contain 'Get-EC2Instance'
       (Get-AWSCmdletName -ApiOperation describeinstances -Service "Amazon Elastic Compute Cloud").CmdletName | 
         Should -Contain 'Get-EC2Instance'
     }
   
-    It 'Part 3 - Get-AWSCmdletName with CLI command returns correct results' {
+    It 'Part 1 - Get-AWSCmdletName with CLI command returns correct results' {
       (Get-AWSCmdletName -AwsCliCommand "aws iam list-users").CmdletName | 
         Should -Contain 'Get-IAMUserList'
       (Get-AWSCmdletName -AwsCliCommand "aws ec2 describe-instances").CmdletName | 
@@ -114,13 +85,17 @@ Describe '507 Labs' {
     }
   }
 
-  Context 'Lab 1.3 - Azure' -Skip:$skipAzure {
+  Context 'Lab 1.3 - JSON' {
+    BeforeAll {
+      $azvm = (Get-Content c:\labfiles\config\azvm.json)
+    }
+
     It 'Part 4/6 - Get-AZVM returns results' {
-      (Get-AzVM).Count | Should -BeGreaterOrEqual 3
+      $azvm.Count | Should -BeGreaterOrEqual 3
     }
 
     It 'Part 6 - jq processes az vm output' {
-      $azvm = (az vm list)
+      
       $prop = (($azvm | jq '[ .[] | { vmname: .name, os: .storageProfile.osDisk.osType, vmsize: .hardwareProfile.vmSize, tags: .tags }]' | 
             ConvertFrom-Json) | Get-Member -Type Properties).Name
       $prop | Should -Contain 'os'
@@ -130,7 +105,6 @@ Describe '507 Labs' {
     }
 
     It 'Part 7 - Powershell converts JSON correctly' {
-      $azvm = (az vm list)
       (($azvm | ConvertFrom-Json) | Where-Object Name -Like '*aud507*').Count | 
         Should -BeGreaterOrEqual 3
     }
@@ -164,7 +138,7 @@ Describe '507 Labs' {
       $totalCount = (Get-EC2Instance).Count
       $nonCompliantCount = (Get-EC2Instance |
           Where-Object {
-          ($_.Instances.tags | Where-Object Key -EQ 'Business_Unit').Count -lt 1
+            ($_.Instances.tags | Where-Object Key -EQ 'Business_Unit').Count -lt 1
           }).Count
       $totalCount | Should -Be 5
       $nonCompliantCount | Should -Be 3
@@ -173,55 +147,34 @@ Describe '507 Labs' {
     }
   }
 
-  Context 'Lab 1.4 - Azure' -Skip:$skipAzure {
-    BeforeAll {
-      #ensure the resource graph extension and module are installed
-      az extension add --name resource-graph
-      Import-Module Az.ResourceGraph
-    }
-
-    It 'Part 4 - Resource graph extension is installed' {
-      (az extension list | ConvertFrom-Json).name | Should -Contain 'resource-graph'
-    }
-
-    It 'Part 4 - Resource graph query returns multiple objects' {
-      (az graph query -q 'Resources' | ConvertFrom-Json).Count | Should -BeGreaterThan 20
-    }
-
-    It 'Part 4 - PowerShell graph query returns multiple objects' {
-      $q = 'Resources | order by type | project location, name, type, tags, sku, id'
-      $inventory = Search-AzGraph -Query $q
-      $inventory.Count | Should -BeGreaterThan 10     
-    }
-  }
-
   Context 'Lab 2.1' {
     It 'Part 1 - 6 local users returned' {
-      (Get-LocalUser).Count | Should -Be 6
+      (Get-LocalUser).Count | Should -Be 5
     }    
 
-    It 'Part 2 - Student and sshd are the only enabled users' {
+    It 'Part 2 - Student and rciadmin are the only enabled users' {
       $enabledUsers = (Get-LocalUser | Where-Object enabled -EQ $true)
       $enabledUsers.Count | Should -Be 2
       $enabledUsers.Name | Should -Contain 'student'
-      $enabledUsers.Name | Should -Contain 'sshd'      
+      $enabledUsers.Name | Should -Contain 'rciadmin'      
     }
 
-    It 'Part 2 - WDAGUtilityAccount is the only PasswordExires users' {
+    It 'Part 2 - WDAGUtilityAccount and student are the only PasswordExpires users' {
       $passwordExiresUsers = (Get-LocalUser | Where-Object passwordExpires -NE $null)
-      $passwordExiresUsers.Count | Should -Be 1
+      $passwordExiresUsers.Count | Should -Be 2
       $passwordExiresUsers.Name | Should -Contain 'WDAGUtilityAccount' 
+      $passwordExiresUsers.Name | Should -Contain 'student' 
     }
 
     It 'Part 3 - Not enabled users' {
       $enabledUsers = (Get-LocalUser | Where-Object enabled -EQ $false)
-      $enabledUsers.Count | Should -Be 4
+      $enabledUsers.Count | Should -Be 3
     }
   }
 
   Context 'Lab 2.2' {
-    It 'Part 1 - Build number is 19045' {
-      (Get-CimInstance Win32_OperatingSystem).BuildNumber | Should -Be 19045
+    It 'Part 1 - Build number is 26100' {
+      (Get-CimInstance Win32_OperatingSystem).BuildNumber | Should -Be 26100
     }
 
     It 'Part 1 - At least one hotfix returns' {
@@ -284,8 +237,8 @@ Describe '507 Labs' {
 
     It 'Part 2 - Get-LocalGroupMember returns correct admins' {
       $res = (Get-LocalGroupMember -Group "administrators")
-      $res.Name | Should -Contain '507WIN10\Administrator'
-      $res.Name | Should -Contain '507WIN10\Student'
+      $res.Name -replace ".*\\" | Should -Contain '507WIN10\rciadmin'
+      $res.Name -replace ".*\\" | Should -Contain '507WIN10\Student'
     }
 
     It 'Part 2 - UserRights.psm1 returns admin for debug privilege' {
